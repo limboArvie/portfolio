fa3a6a3c3da058eef22c922ba436a3da4db5eba8
Arvie Limbo <arvie.limbo@cognizant.com>
2023-12-09 20:45:48 +0800

    Updated js file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function makeActive(event) {
+    var previous = document.getElementsByClassName("active");
+    if (previous.length > 0) {
+       previous[0].className = previous[0].className.replace(" active", "");
+    }
+    event.target.className += " active";
+ }
